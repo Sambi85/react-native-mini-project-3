97f4283fb52eac5fe0156b24581770be8892fae2
github username <github email address>
2021-02-20 18:56:39 -0500

    add-blog-post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
@@ -6,12 +6,13 @@ const BlogContext = React.createContext();
 // children: react feature, it's a prop
 
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: 'Blog Post # 1'},
-        { title: 'Blog Post # 2'}
-    ]
+   const [blogPosts, setBlogPosts] = useState([]);
 
-    return <BlogContext.Provider value={blogPosts}>
+   const addBlogPost = () => {
+       setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]);
+   }
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
         { children }
     </BlogContext.Provider>;
 }
