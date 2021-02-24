3effafdff76896b47c393214ce34a762f0a0a919
github username <github email address>
2021-02-24 18:51:27 -0500

    add-blog-post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,16 +4,23 @@ const BlogContext = React.createContext();
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        
         case 'add_blogpost':
-         return [...state, { title: `Blog Post #${state.length + 1}` }];   
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+        
+        default: 
+            return state;   
     }
 }
 
 export const BlogProvider = ({ children }) => {
    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
+   const addBlogPost = () => {
+       dispatch({ type: 'add_blogpost' });
+   }
 
-    return <BlogContext.Provider value={{ data: blogPosts }}>
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
         { children }
     </BlogContext.Provider>;
 }
