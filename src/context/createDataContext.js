06539d48b589efe52e3abf99718a4bca332b21e7
github username <github email address>
2021-03-01 14:42:15 -0700

    reftactor-dispatch

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -4,6 +4,12 @@ export default (reducer, actions, intitalState) => {
     
     const Context = React.createContext();
     
+    const boundActions = {};
+
+    for (let key in actions) {
+        boundActions[key] = actions[key](disptach);
+    }
+
     const Provider = ({ children }) => {
         const [state, dispatch] = useReducer(reducer, intitalState);
     
